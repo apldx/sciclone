e23acbb46e28a0e066a8f4541aa9cc5559adc629
Ajay Khanna <appliedstochastics@gmail.com>
2022-02-14 11:00:50 -0600

    Update

--- R/clustering.R
+++ R/clustering.R
@@ -1526,6 +1526,7 @@ binomial.bmm.filter.clusters <- function(vafs.merged, vafs, successes, total.tri
     indices.to.keep <- rep(TRUE, N.c)
     if((apply.min.items.condition == TRUE) & (N.c > 1)) {
       threshold.pts <- max(3, ceiling(.005*N))
+      threshold.pts <- 2
 
       num.items.per.cluster <- rep(0, N.c)
       for(n in 1:N) {
